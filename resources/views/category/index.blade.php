37f0595b1154db180ebfc61b9f28d55900b15ba3
Ebenezer Omeonu <ebene2009@gmail.com>
2021-03-04 16:48:46 +0100

    create category

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,6 @@
+@include('layouts.home')
+
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+
+Route::prefix('categories')->middleware(['auth'])->group(function () {
+
+    Route::get('/', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('new', [CategoryController::class, 'create'])->name('category.create');
+    Route::post('create', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::post('{category}/update', [CategoryController::class, 'update'])->name('category.update');
+    Route::get('{category}/delete', [CategoryController::class, 'destroy'])->name('category.delete');
+
+});
+
+
+
+
+
+require __DIR__.'/auth.php';
