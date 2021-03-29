7d93acf5c19dc713d1ef19bbcd13ef6d8928d840
unknown <ebene2009@gmail.com>
2021-03-29 22:10:06 +0100

    fix company data seed file

--- database/seeders/CompanyTableSeeder.php
+++ database/seeders/CompanyTableSeeder.php
@@ -20,7 +20,7 @@ class CompanyTableSeeder extends Seeder
             'about'=>'This is the Fash9ja Gist, an indegenious nigerian fashion and lifestyle blog where you find more than enough about the ups and downs in life and all the beautiful things it has to offer,further more you get to experience the fashion world,lifestyle,travels,events and more in whole new dimension,giving you all the help and inspiration that you need to keep you on the fab lane.',
             'email'=>'ebenechi1@gmail.com',
             'phone'=>'+2348163159720',
-            'Address'=>' 1 Awolowo way, VI, Lagos, Nigeria'
+            'address'=>' 1 Awolowo way, VI, Lagos, Nigeria'
         ]);
         //
     }
